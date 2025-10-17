22dbcd9c31ada5e0b70b766ee2aea13d72d13675
spappalardo <jspkay@gmail.com>
2025-10-17 13:37:44 +0200

    Added randr script for multimonitor setups

--- .config/sway/scripts/randr_screens.sh
+++ .config/sway/scripts/randr_screens.sh
@@ -1,7 +1,10 @@
 #!/bin/zsh
 
 homescreen=$(randr | grep "15B1")
-if [ $? ]; then
+if [ $? -eq 0 ]; then # We are at home 
+  echo "We are in"
   out=$(echo $homescreen | cut -d' ' -f1)
   randr --output $out --right-of eDP-1
 fi
+
+
